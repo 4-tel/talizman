1b6888840a849a8e56fb0f892083bdc14eaf6359
Dominik Malek <dominik.malek@techmo.pl>
2022-06-11 00:16:29 +0200

    plansza (vol2 in progress)

--- app/views/script/models.js
+++ app/views/script/models.js
@@ -89,18 +89,88 @@ class Models {
             },
             {
                 position: {
-                    x: -2000,
+                    x: -2050,
                     y: -1130
                 },
                 height: 370,
-                width: 570,
+                width: 470,
+                material: this.first_land
+
+            },
+            {
+                position: {
+                    x: -1510,
+                    y: -1130
+                },
+                height: 370,
+                width: 610,
+                material: this.second_land
+
+            },
+            {
+                position: {
+                    x: -910,
+                    y: -1130
+                },
+                height: 370,
+                width: 610,
+                material: this.second_land
+
+            },
+            {
+                position: {
+                    x: -300,
+                    y: -1130
+                },
+                height: 370,
+                width: 610,
+                material: this.second_land
+
+            },
+            {
+                position: {
+                    x: 310,
+                    y: -1130
+                },
+                height: 370,
+                width: 610,
+                material: this.second_land
+
+            },
+            {
+                position: {
+                    x: 920,
+                    y: -1130
+                },
+                height: 370,
+                width: 610,
+                material: this.second_land
+
+            },
+            {
+                position: {
+                    x: 1470,
+                    y: -1130
+                },
+                height: 370,
+                width: 470,
+                material: this.first_land
+
+            },
+            {
+                position: {
+                    x: -2050,
+                    y: -760
+                },
+                height: 370,
+                width: 470,
                 material: this.first_land
 
             },
             {
                 position: {
                     x: -1430,
-                    y: -1130
+                    y: -760
                 },
                 height: 370,
                 width: 570,
@@ -110,76 +180,6 @@ class Models {
             {
                 position: {
                     x: -860,
-                    y: -1130
-                },
-                height: 370,
-                width: 570,
-                material: this.second_land
-
-            },
-            {
-                position: {
-                    x: -290,
-                    y: -1130
-                },
-                height: 370,
-                width: 570,
-                material: this.second_land
-
-            },
-            {
-                position: {
-                    x: 280,
-                    y: -1130
-                },
-                height: 370,
-                width: 570,
-                material: this.second_land
-
-            },
-            {
-                position: {
-                    x: 850,
-                    y: -1130
-                },
-                height: 370,
-                width: 570,
-                material: this.second_land
-
-            },
-            {
-                position: {
-                    x: 1420,
-                    y: -1130
-                },
-                height: 370,
-                width: 570,
-                material: this.first_land
-
-            },
-            {
-                position: {
-                    x: -2000,
-                    y: -760
-                },
-                height: 370,
-                width: 570,
-                material: this.first_land
-
-            },
-            {
-                position: {
-                    x: -1430,
-                    y: -760
-                },
-                height: 370,
-                width: 570,
-                material: this.second_land
-
-            },
-            {
-                position: {
-                    x: -860,
                     y: -760
                 },
                 height: 370,
@@ -219,21 +219,21 @@ class Models {
             },
             {
                 position: {
-                    x: 1420,
+                    x: 1470,
                     y: -760
                 },
                 height: 370,
-                width: 570,
+                width: 470,
                 material: this.first_land
 
             },
             {
                 position: {
-                    x: -2000,
+                    x: -2050,
                     y: -390
                 },
                 height: 370,
-                width: 570,
+                width: 470,
                 material: this.first_land
 
             },
@@ -289,21 +289,21 @@ class Models {
             },
             {
                 position: {
-                    x: 1420,
+                    x: 1470,
                     y: -390
                 },
                 height: 370,
-                width: 570,
+                width: 470,
                 material: this.first_land
 
             },
             {
                 position: {
-                    x: -2000,
+                    x: -2050,
                     y: -20
                 },
                 height: 370,
-                width: 570,
+                width: 470,
                 material: this.first_land
 
             },
@@ -359,21 +359,21 @@ class Models {
             },
             {
                 position: {
-                    x: 1420,
+                    x: 1470,
                     y: -20
                 },
                 height: 370,
-                width: 570,
+                width: 470,
                 material: this.first_land
 
             },
             {
                 position: {
-                    x: -2000,
+                    x: -2050,
                     y: 350
                 },
                 height: 370,
-                width: 570,
+                width: 470,
                 material: this.first_land
 
             },
@@ -429,11 +429,11 @@ class Models {
             },
             {
                 position: {
-                    x: 1420,
+                    x: 1470,
                     y: 350
                 },
                 height: 370,
-                width: 570,
+                width: 470,
                 material: this.first_land
 
             },
