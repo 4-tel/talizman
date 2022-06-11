ea6f0f0bc5af3fd19a439ef1f0edfc05bc8c2134
Dominik Malek <dominik.malek@techmo.pl>
2022-06-12 01:13:57 +0200

    ghbzws

--- app/views/script/startGame.js
+++ app/views/script/startGame.js
@@ -8,14 +8,14 @@ class StartGame {
     }
     choseYourChampion = (champions) => {
         let time_To_Pick = document.createElement("div")
-        time_To_Pick.style = 'position: absolute; width: 100%; height: 100%; background-color: green; z-index: 2;'
-        for (let i = 0; champions.length; i++) {
-            let card = `<div id="card" style = "float: "left"; background-color: "red"; width= 300px; height = 300px;">`
-            time_To_Pick.appendChild(card)
+        time_To_Pick.style = 'position: absolute; top: 0px;width: 100%; height: 100%; background-color: #00ff0050; z-index: 2;'
+        for (let i = 0; i < Object.keys(champions).length; i++) {
+            let card = `<div id="card" style = "float: left; background-color: red; width= 300px; height = 300px;">`
+            time_To_Pick.innerHTML += (card)
         }
         document.getElementById("game").appendChild(time_To_Pick)
     }
     greeting = () => {
-        document.getElementById("game").innerHTML += `<h1>Hello ${this.username}! You play as ${this.hero}`
+        document.getElementById("game").innerHTML += `<h1>Hello ${this.username}! You play as ${this.hero}</h1>`
     }
 }
