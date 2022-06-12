742bdb6513ea8c3f5bf3e0c98f29b09c8f9388e9
Dominik Malek <dominik.malek@techmo.pl>
2022-06-12 16:18:33 +0200

    ghtna tak

--- app/views/script/startGame.js
+++ app/views/script/startGame.js
@@ -1,21 +1,31 @@
 class StartGame {
     constructor(player) {
         this.player = player
-        this.username = this.player.username
+        this.username = this.player
         this.champion_pool = new Heroes()
         this.choseYourChampion(this.champion_pool.get())
-        //this.greeting()
     }
     choseYourChampion = (champions) => {
         let time_To_Pick = document.createElement("div")
+        time_To_Pick.id = "champion_select"
         time_To_Pick.style = 'position: absolute; top: 0px;width: 100%; height: 100%; background-color: #00ff0050; z-index: 2;'
         for (let i = 0; i < Object.keys(champions).length; i++) {
-            let card = `<div id="card" style = "float: left; background-color: red; width= 300px; height = 300px;">`
+            let card = `<div onclick="game.start.revail(${i})"id="card${i}" style = "float: left; background-color: red; width: 300px; height: 300px; margin: 60px;">`
             time_To_Pick.innerHTML += (card)
         }
         document.getElementById("game").appendChild(time_To_Pick)
     }
-    greeting = () => {
-        document.getElementById("game").innerHTML += `<h1>Hello ${this.username}! You play as ${this.hero}</h1>`
+    greeting = (hero) => {
+        let greeting = document.createElement("h1")
+        greeting.innerHTML = `Hello ${this.username}! You play as ${hero}`
+        document.getElementById("game").appendChild(greeting)
+    }
+    revail = async (nr) => {
+        document.getElementById(`card${nr}`).innerHTML = Object.keys(this.champion_pool.get())[nr]
+        await game.sleep(1000)
+        console.log("aaa")
+        document.getElementById("champion_select").parentNode.removeChild(document.getElementById("champion_select"))
+        let hero = Object.values(this.champion_pool.get())[nr].name
+        this.greeting(hero)
     }
 }
