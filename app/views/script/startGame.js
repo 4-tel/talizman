fd523f107e96ee101e7fd6163aeef3f38bed89ab
oskar <oskarm0503@gmail.com>
2022-06-13 01:18:20 +0200

    cards

--- app/views/script/startGame.js
+++ app/views/script/startGame.js
@@ -13,9 +13,12 @@ class StartGame {
         let time_To_Pick = document.createElement("div")
         time_To_Pick.id = "cardsMenu"
         time_To_Pick.innerHTML = `<p>Time to pick</p>`
+        let cards = document.createElement("div")
+        cards.id = "cards"
+        time_To_Pick.append(cards)
         for (let i = 0; i < Object.keys(champions).length; i++) {
-            let card = `<div onclick="game.start.revail(${i})"id="card${i}">`
-            time_To_Pick.innerHTML += (card)
+            let card = `<div onclick="game.start.reveal(${i})"id="card${i}">`
+            cards.innerHTML += (card)
         }
         document.getElementById("game").appendChild(time_To_Pick)
     }
@@ -28,10 +31,24 @@ class StartGame {
     }
 
 
-    revail = async (nr) => {
+    reveal = async (nr) => {
+
+        document.getElementById("cards").children[nr].style.width = '0vw'
+        document.getElementById("cards").children[nr].style.marginLeft = '15%'
+        document.getElementById("cards").children[nr].style.marginRight = '15%'
+
+        setTimeout(() => {
+
+            document.getElementById("cards").children[nr].style.marginLeft = '5%'
+            document.getElementById("cards").children[nr].style.marginRight = '5%'
+            document.getElementById("cards").children[nr].style.backgroundImage = 'none'
+            document.getElementById("cards").children[nr].style.width = '13.9vw'
+            document.getElementById("cards").children[nr].style.border = '1px solid black'
+
+        }, 250)
+
         document.getElementById(`card${nr}`).innerHTML = Object.keys(this.champion_pool.get())[nr]
-        await game.sleep(1000)
-        console.log("aaa")
+        await game.sleep(3000)
         document.getElementById("cardsMenu").parentNode.removeChild(document.getElementById("cardsMenu"))
         let hero = Object.values(this.champion_pool.get())[nr].name
         this.greeting(hero)
