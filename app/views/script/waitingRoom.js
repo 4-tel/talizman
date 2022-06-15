a4c4a08b381d4679167feb019cae11d32ec7709a
oskar <oskarm0503@gmail.com>
2022-06-15 16:38:53 +0200

    niepamieatm

--- app/views/script/waitingRoom.js
+++ app/views/script/waitingRoom.js
@@ -13,8 +13,16 @@ class WaitingRoom {
         div.id = "waitingRoom"
         div.innerHTML = `
         <p>session id:</p><p id="session_id">${await this.get_session_data()}</p>
-        <div id="players"><p style="font-size:2.5vh">users in session:</p><hr /><div>${await this.get_users_html()}</div></div><button onclick="waitingRoom.leave()">start game</button>`
+        <div id="players"><p style="font-size:2.5vh">users in session:</p><hr /><div>${await this.get_users_html()}</div></div><p style="font-size:1.5vh">You can press tab to view player in game</p><button onclick="waitingRoom.leave()">start game</button>`
         document.body.append(div)
+
+        document.getElementById('game').style.marginLeft = "10vw"
+
+        let curtain = document.createElement('div')
+        curtain.id = 'curtain'
+        curtain.innerHTML = `<p>Waiting for more players to join...</p><img src="textures/loading.gif">`
+        document.body.append(curtain)
+
 
     }
 
@@ -22,7 +30,7 @@ class WaitingRoom {
     async get_users_html() {
 
         try {
-            let output
+            let output = ''
             let users = JSON.parse(await net.sessionInfo(await this.get_session_data())).users
 
             for (let el of users) {
@@ -49,6 +57,8 @@ class WaitingRoom {
     async leave() {
 
         document.getElementById('waitingRoom').remove()
+        document.getElementById('curtain').remove()
+        document.getElementById('game').style.marginLeft = "0vw"
         game.start = new StartGame(JSON.parse(await new Net().getUsername(document.cookie.split("=")[1])).username)
         game.start.init()
 
