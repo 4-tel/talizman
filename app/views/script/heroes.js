f740dd87a2dce00f580ae0b9d8e127363ce11c27
Dominik Malek <dominik.malek@techmo.pl>
2022-06-16 11:54:52 +0200

    heroes

--- app/views/script/heroes.js
+++ app/views/script/heroes.js
@@ -45,5 +45,170 @@ const heroes = {
         special_abilites: {
             /// nie wiem jak xd
         }
+    },
+    dwarf: {
+        img: '/textures/dwarf.jpg',
+        name: "dwarf",
+        stats: {
+            strength: 3,
+            mana: 3,
+            luck: 5,
+            health: 5
+        },
+        character: "neutral",
+        starting_tile: "rocks",
+        special_abilites: {
+            /// nie wiem jak xd
+        }
+    },
+    fairy: {
+        img: '/textures/fairy.jpg',
+        name: "fairy",
+        stats: {
+            strength: 2,
+            mana: 4,
+            luck: 2,
+            health: 4
+        },
+        character: "good",
+        starting_tile: "chapel",
+        special_abilites: {
+            /// nie wiem jak xd
+        }
+    },
+    assasin: {
+        img: '/textures/assasin.jpg',
+        name: "assasin",
+        stats: {
+            strength: 3,
+            mana: 3,
+            luck: 3,
+            health: 4
+        },
+        character: "bad",
+        starting_tile: "city",
+        special_abilites: {
+            /// nie wiem jak xd
+        }
+    },
+    preist: {
+        img: '/textures/preist.jpg',
+        name: "preist",
+        stats: {
+            strength: 2,
+            mana: 4,
+            luck: 5,
+            health: 4
+        },
+        character: "good",
+        starting_tile: "chapel",
+        special_abilites: {
+            /// nie wiem jak xd
+        }
+    },
+    monk: {
+        img: '/textures/monk.jpg',
+        name: "monk",
+        stats: {
+            strength: 2,
+            mana: 3,
+            luck: 5,
+            health: 4
+        },
+        character: "good",
+        starting_tile: "village",
+        special_abilites: {
+            /// nie wiem jak xd
+        }
+    },
+    ghoul: {
+        img: '/textures/ghoul.jpg',
+        name: "ghoul",
+        stats: {
+            strength: 2,
+            mana: 4,
+            luck: 4,
+            health: 4
+        },
+        character: "bad",
+        starting_tile: "cementery",
+        special_abilites: {
+            /// nie wiem jak xd
+        }
+    },
+    wizard: {
+        img: '/textures/wizard.jpg',
+        name: "wizard",
+        stats: {
+            strength: 2,
+            mana: 5,
+            luck: 3,
+            health: 4
+        },
+        character: "bad",
+        starting_tile: "cementery",
+        special_abilites: {
+            /// nie wiem jak xd
+        }
+    },
+    witch: {
+        img: '/textures/witch.jpg',
+        name: "witch",
+        stats: {
+            strength: 2,
+            mana: 4,
+            luck: 3,
+            health: 4
+        },
+        character: "bad",
+        starting_tile: "cementery",
+        special_abilites: {
+            /// nie wiem jak xd
+        }
+    },
+    druid: {
+        img: '/textures/druid.jpg',
+        name: "druid",
+        stats: {
+            strength: 2,
+            mana: 4,
+            luck: 4,
+            health: 4
+        },
+        character: "neutral",
+        starting_tile: "forest",
+        special_abilites: {
+            /// nie wiem jak xd
+        }
+    },
+    minstrel: {
+        img: '/textures/minstrel.jpg',
+        name: "minstrel",
+        stats: {
+            strength: 2,
+            mana: 4,
+            luck: 5,
+            health: 4
+        },
+        character: "good",
+        starting_tile: "tavern",
+        special_abilites: {
+            /// nie wiem jak xd
+        }
+    },
+    elf: {
+        img: '/textures/elf.jpg',
+        name: "elf",
+        stats: {
+            strength: 3,
+            mana: 4,
+            luck: 3,
+            health: 4
+        },
+        character: "good",
+        starting_tile: "forest",
+        special_abilites: {
+            /// nie wiem jak xd
+        }
     }
 }
